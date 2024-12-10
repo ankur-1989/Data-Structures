4d24a01cac3e262627f118031a9bfa5c517567b3
Ankur Gupta <ankurgupta@web.de>
2024-12-10 11:38:21 +0100

    fixed one correction.

--- data-structures/leetcode/bit-manipulation/paying-up.js
+++ data-structures/leetcode/bit-manipulation/paying-up.js
@@ -1,7 +1,8 @@
 function isPaying(arr, demand) {
   const max = 1 << arr.length;
-  let subsetSum = 0;
+  
   for (let k = 1; k < max; k++) {
+    let subsetSum = 0;
     for (let i = 0; i < arr.length; i++) {
       if (k & (1 << i)) {
         subsetSum += arr[i];
@@ -9,7 +10,6 @@ function isPaying(arr, demand) {
     }
 
     if (subsetSum === demand) return true;
-    subsetSum = 0;
   }
   return false;
 }
