666cef2ee5ab02491cccc5d7c296954384ca8fb6
Ankur Gupta <ankurgupta@web.de>
2025-01-05 23:53:17 +0100

    leetcode problem 144 solved.

---
+++ leetcode/tree/lc-144.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function (root, result = []) {
+  // DLR - Data Left Right
+  if (root == null) return result;
+  result.push(root.val);
+  if (root.left) preorderTraversal(root.left, result);
+  if (root.right) preorderTraversal(root.right, result);
+  return result;
+};
