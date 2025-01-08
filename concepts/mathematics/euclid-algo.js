7c948e84d7026e9af99abc2523bdb133acdc3870
Ankur Gupta <ankurgupta@web.de>
2025-01-08 15:22:26 +0100

    Add: recursive implementation of GCD of two numbers

--- concepts/mathematics/euclid-algo.js
+++ concepts/mathematics/euclid-algo.js
@@ -10,3 +10,19 @@ function gcd(a, b) {
 }
 
 console.log(gcd(105, 350));
+
+// recursive approach
+const gcdR = (num1, num2) => {
+  if(num1 === num2) return num1;  
+  if(num2 === 0) return num1;
+  if(num1 < num2) {
+    const temp = num1;
+    num1 = num2;
+    num2 = temp;
+    return gcdR(num2, num1 % num2);
+  } else {
+       return gcdR(num2, num1 % num2);
+  }
+}
+
+console.log(gcdR(105, 350));
