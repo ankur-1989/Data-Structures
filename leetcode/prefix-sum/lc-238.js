d1bd27056bd227ec77a304417c06602e35b214d8
Ankur Gupta <ankurgupta@web.de>
2025-01-09 20:15:23 +0100

    Add: solved leetcode challenge 238

---
+++ leetcode/prefix-sum/lc-238.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function (nums) {
+  let answer = [];
+  answer[0] = 1;
+  for (let i = 1; i < nums.length; i++) {
+    answer[i] = nums[i - 1] * answer[i - 1];
+  }
+  let suffixProduct = 1;
+  for (let i = nums.length - 2; i >= 0; i--) {
+    suffixProduct *= nums[i + 1];
+    answer[i] = answer[i] * suffixProduct;
+  }
+  return answer;
+};
